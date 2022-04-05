6484c36de4e80ff401c861976cd2b1e5db70c89b
Sohang Chopra <sohangchopra@gmail.com>
2022-04-05 16:23:11 +0530

    Simplify Code [UNTESTED]
    Use Fetch API instead of XMLHttpRequest

--- static/js/script.js
+++ static/js/script.js
@@ -13,35 +13,22 @@ async function login() {
     ctx.drawImage(webcam, 0, 0, canvas.width, canvas.height);
     const imageUrl = canvas.toDataURL('image/jpeg');
     try {
-        console.log(await ajax('POST', '/login', { imageUrl }, json=true));
+        await postJSON('/login', { imageUrl });
         alert('User Logged in!')
     } catch (error) {
         if (error.status == 401) {  // unauthorized
             username = prompt('NEW USER\nYou have not registered yet.\nPlease enter username to register now')
-            await ajax('POST', '/register', { username, imageUrl }, json=true)
+            await postJSON('/register', { username, imageUrl });
         }
     }
 };
 
-function ajax(method, url, data, json=False) {
-    return new Promise(function (resolve, reject) {
-        let xhr = new XMLHttpRequest();
-        xhr.open(method, url);
-        xhr.onload = function () {
-            if (this.status >= 200 && this.status < 300) {
-                resolve(xhr.response);
-            } else {
-                reject({status: this.status, statusText: xhr.statusText});
-            }
-        };
-        xhr.onerror = function () {
-            reject({status: this.status, statusText: xhr.statusText});
-        };
-        if (json) {
-            xhr.setRequestHeader('Content-Type', 'application/json');
-            xhr.send(JSON.stringify(data));
-        } else {
-            xhr.send(data)
-        }
+async function postJSON(url, data) {
+    await fetch(url, {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(data)
     });
 }
