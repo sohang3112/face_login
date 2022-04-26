ee961f41513fe62cf6d275c6f42231ebe65bf369
Sohang  Chopra <sohang.chopra@mfilterit.com>
2022-04-26 17:32:16 +0530

    Simplified decoding of data url using urllib library

--- main.py
+++ main.py
@@ -7,18 +7,17 @@ from fastapi.staticfiles import StaticFiles
 from pydantic import BaseModel
 from starlette.responses import FileResponse
 import numpy as np
+from urllib.request import urlopen
 import face_recognition as fc
 from PIL import Image
 
 
 FACE_DISTANCE_TOLERANCE = 0.6
 
-def image_from_base64(string: str) -> Image:
-    img_bytes = base64.decodebytes(bytes(string, 'ascii'))
+def image_from_url(data_url: str) -> Image:
+    with urlopen(data_url) as response:
+        img_bytes = response.read()
     return Image.open(BytesIO(img_bytes))
-
-def jpeg_image_from_url(url: str) -> Image:
-    return image_from_base64(url.removeprefix('data:image/jpeg;base64,'))
 
 def face_encoding_from_image(img: Image):
     return fc.face_encodings(np.array(img))[0]
@@ -40,7 +39,7 @@ async def register(req: Request):
     # TODO - check that it doesn't already exist!
     json = await req.json()
     username = json['username']
-    enc = face_encoding_from_image(jpeg_image_from_url(json['imageUrl']))
+    enc = face_encoding_from_image(image_from_url(json['imageUrl']))
     users.append({'username': username, 'data': '', 'face_encoding': enc})
 
 @app.post('/login')
